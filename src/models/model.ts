63487a8a331ec9e0bf688c94d0c3e899413a0f91
migmatore <kazakvova201@gmail.com>
2022-04-26 22:29:31 +0300

    Create api models

--- src/models/model.ts
+++ src/models/model.ts
@@ -1,3 +1,70 @@
-export interface S {
-    count: number
+export interface Group {
+    id: number,
+    groupName: string
+}
+
+export interface GroupResponse {
+    status: number,
+    request: string,
+    response: Array<Group>
+}
+
+export interface Subject {
+    id: number,
+    subjectName: string
+}
+
+export interface SubjectResponse {
+    status: number,
+    request: string,
+    response: Array<Subject>
+}
+
+export interface Teacher {
+    id: number,
+    teacherName: string
+}
+
+export interface TeacherResponse {
+    status: number,
+    request: string,
+    response: Array<Teacher>
+}
+
+export interface Weekday {
+    id: number,
+    weekdayName: string
+}
+
+export interface WeekdayResponse {
+    status: number,
+    request: string,
+    response: Array<Weekday>
+}
+
+export interface Office {
+    id: number,
+    officeNumber: string
+}
+
+export interface OfficeResponse {
+    status: number,
+    request: string,
+    response: Array<Office>
+}
+
+export interface Shedule {
+    id: number,
+    date: string,
+    group: Group,
+    Weekday: Weekday,
+    subject: Subject,
+    office: Office,
+    teacher: Teacher
+}
+
+export interface SheduleResponse {
+    status: number,
+    request: string,
+    response: Array<Shedule>
 }
