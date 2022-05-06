7946c6c845d480ae342b9cd64edca5cd55be19f1
kazuki <ErickKrause2004@yandex.ru>
2022-05-06 11:18:05 +0300

    Add header

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,12 @@
 <template>
-	<div class="common-layout">
-		<el-container>
-			<el-aside width="200px">Aside</el-aside>
-			<el-container>
-				<el-header>Header</el-header>
-				<el-main>Main</el-main>
-			</el-container>
-		</el-container>
-	</div>
-
-	<button @click="getG">
-		
-	</button>
-	<li v-for="g in group" :key="g.id">
-		{{ g.id }} - {{ g.group_name }}
-	</li>
+<nav class="navbar navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand oil-logo" href="#">
+      <img src="../svg/gnt-oil_logo.svg" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy">
+      GNT-OIL
+    </a>
+  </div>
+</nav>
 </template>
 
 <script lang="ts">
@@ -24,42 +16,54 @@ import { api } from './api/api'
 import axios from 'axios'
 import { GroupResponse } from './models/model'
 
-type group = {
-	id: number;
-	group_name: string;
-}
+// type group = {
+// 	id: number;
+// 	group_name: string;
+// }
 
 export default class App extends Vue {
-	mainStore = useMainStore();
-	count = 0;
-	public group: Array<group> = [];
+	// mainStore = useMainStore();
+	// count = 0;
+	// public group: Array<group> = [];
 
-	mounted() {
-		this.getG();
-	}
+	// mounted() {
+	// 	this.getG();
+	// }
 
-	increment() {
-		console.log("increment");
-		this.mainStore.increment();
-		this.count = this.mainStore.getN();
-	}
+	// increment() {
+	// 	console.log("increment");
+	// 	this.mainStore.increment();
+	// 	this.count = this.mainStore.getN();
+	// }
 
-	async getG() {
-		await api.group.getAll().then(resp => {
-			// for (let i = 0; i < resp.data.response.length; i++) {
-			// 	this.group[i].group_name = resp.data.response[i].group_name
-			// 	// this.group.push(resp.data.response)
-			// }
+	// async getG() {
+	// 	await api.group.getAll().then(resp => {
+	// 		// for (let i = 0; i < resp.data.response.length; i++) {
+	// 		// 	this.group[i].group_name = resp.data.response[i].group_name
+	// 		// 	// this.group.push(resp.data.response)
+	// 		// }
 
-			resp.data.response.forEach(d => {
-				this.group.push(d)
-			})
-		})
+	// 		resp.data.response.forEach(d => {
+	// 			this.group.push(d)
+	// 		})
+	// 	})
 
-		console.log(this.group)
-	}
+	// 	console.log(this.group)
+	// }
 }
 </script>
 
 <style lang="scss">
+
+@font-face {
+	font-family: 'Garet';
+	src: url('../fonts/Garet/Garet-Heavy.otf') format('opentype');
+	font-style: normal;
+	font-weight: 850;
+}
+
+.oil-logo {
+	font-family: "Garet";
+}
+
 </style>
--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,6 @@ import router from './router'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
+import 'bootstrap/scss/bootstrap.scss'
 
-createApp(App).use(createPinia()).use(router).use(VueAxios, axios).use(ElementPlus).mount('#app')
+createApp(App).use(createPinia()).use(router).use(VueAxios, axios).mount('#app')
